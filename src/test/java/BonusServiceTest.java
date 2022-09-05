f9adb3355392e7f0f60409017265431b3b4fbfdb
Vladimir Odintcev <vovaodintcev@gmail.com>
2022-09-05 19:55:10 +0500

    add file

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,26 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class BonusServiceTest {
+
+
+
+    @Test
+    public void test() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 500;
+
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+}
